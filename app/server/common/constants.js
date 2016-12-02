56697b873104e7ad841c2c01771fd9d1a9f412e2
gecapo <georgekalinkov@gmail.com>
2016-12-02 18:06:35 +0200

    Inital constats update
    To be added description

--- app/server/common/constants.js
+++ app/server/common/constants.js
@@ -1,7 +1,6 @@
 module.exports = {
     defaultImageUrl: "https://gitlab.com/uploads/project/avatar/1345047/leaflet-here.png",
-    types: [
-        {
+    types: [{
             test: "test"
         },
         {
@@ -11,8 +10,7 @@ module.exports = {
             dict: "dict"
         }
     ],
-    categories: [
-        {
+    categories: [{
             html: "html"
         },
         {
@@ -58,75 +56,102 @@ module.exports = {
             mongoose: "mongoose"
         }
     ],
-    logos: [
-        {
+    logos: [{
+            title: "HTML",
             name: "html loading...",
             path: "images/logos/html.png",
+            description: "",
             num: "0"
         },
         {
+            title: "CSS",
             name: "css loading...",
             path: "images/logos/css.png",
+            description: "",
             num: "1"
         },
         {
+            title: "Bootsrap",
             name: "bootstrap loading...",
             path: "images/logos/bootstrap.png",
+            description: "",
             num: "2"
         },
         {
+            title: "LESS",
             name: "less loading...",
             path: "images/logos/less.png",
+            description: "",
             num: "3"
         },
         {
+            title: "SASS",
             name: "sass loading...",
             path: "images/logos/sass.png",
+            description: "",
             num: "4"
         },
         {
+            title: "Stylus",
             name: "stylus loading...",
             path: "images/logos/stylus.png",
+            description: "",
             num: "5"
         },
         {
+            title: "JavaScript",
             name: "javascript loading...",
             path: "images/logos/js.png",
+            description: "",
             num: "6"
         },
         {
+            title: "jQuery",
             name: "jquery loading...",
             path: "images/logos/jquery.png",
+            description: "",
             num: "7"
         },
         {
+            title: "TypeScript",
             name: "typescript loading...",
             path: "images/logos/ts.png",
+            description: "",
             num: "8"
         },
         {
+            title: "Angular",
             name: "angular loading...",
             path: "images/logos/angular.png",
+            description: "",
             num: "9"
         },
         {
+            title: "React",
             name: "react loading...",
             path: "images/logos/react.png",
+            description: "",
             num: "10"
         },
         {
+            title: "NodeJs",
             name: "nodejs loading...",
             path: "images/logos/nodejs.png",
+            description: "",
             num: "11"
         },
         {
+            title: "Express",
             name: "express loading...",
             path: "images/logos/express.png",
+            description: "",
             num: "12"
         },
         {
+            title: "MondoDb",
             name: "mongodb loading...",
             path: "images/logos/mongodb.png",
+            description: "",
             num: "13"
         }
     ]
